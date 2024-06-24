c535a52b13891e07b0b2a3e85b41061d6ad633cc
Atmosoft.LLC <147726001+kunalatmosoft@users.noreply.github.com>
2024-06-24 15:27:32 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="https://as2.ftcdn.net/v2/jpg/04/74/47/55/1000_F_474475514_RvoYQG6un3iEeRAkvLP5ZH0uoEM9HUM2.jpg" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
+
       <body>
         <ThemeProvider
           attribute="class"
